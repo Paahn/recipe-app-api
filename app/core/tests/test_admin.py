d3920dc62322cdd84d377cf370d417ec55b87196
Paahn <pjhatzis@gmail.com>
2020-08-06 18:08:39 -0700

    created a new file to test admin users, and created an admin and a regular user

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,14 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='email@me.com',
+            password='password12345'
+        )
+        self.client.force_login(self.admin_user)
