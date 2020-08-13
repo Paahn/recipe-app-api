be90af7c7df219bda2ae63f40510318121323127
Paahn <pjhatzis@gmail.com>
2020-08-12 18:12:01 -0700

    creating test to see if db is available for connection when django communicates with it
    this test is necessary because postgres may be slower or not ready yet than django's request to it

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available. Db may not
+        be open for connections if django starts faster than the
+        postgres service"""
+        
