6e75c963825c62a2d66b48c8673454661430bc33
Paahn <pjhatzis@gmail.com>
2020-08-12 18:18:48 -0700

    using patch to mock a connection handler for our test
    if there is no connectionHandler error thrown, that means the db is available for connection and therefore we only have to return true

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,4 +11,5 @@ class CommandTests(TestCase):
         """Test waiting for db when db is available. Db may not
         be open for connections if django starts faster than the
         postgres service"""
-        
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as get_item:
+            get_item.return_value = True
