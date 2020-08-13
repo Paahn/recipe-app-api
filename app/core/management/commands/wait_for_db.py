52d6ea2ee8d53c9ea0170cd952d0950c65f41c66
Paahn <pjhatzis@gmail.com>
2020-08-13 08:53:45 -0700

    added changes to satisfy flake8
    had to change method name to stay at 79 chars per line, since changing the indentation wasn't working

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -15,7 +15,8 @@ class Command(BaseCommand):
             try:
                 db_conn = connections['default']
             except OperationalError:
-                self.stdout.write('Database currently unavailable, waiting 1 second...')
+                self.stdout.write('Database currently unavailable, \
+                waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!!!'))
