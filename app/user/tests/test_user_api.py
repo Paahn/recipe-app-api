c0f99b4db9e70749b080db472292507a015f06e9
Paahn <pjhatzis@gmail.com>
2020-08-18 21:34:03 -0700

    make sure the password is not in the response object
    it is not safe to return the password in the response, even the encrypted version

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -32,3 +32,4 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**response.data)
         self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', response.data)
