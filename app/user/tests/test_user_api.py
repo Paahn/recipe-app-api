9c20ef5db66a518214528f456146ae77b5dfccba
Paahn <pjhatzis@gmail.com>
2020-08-18 21:39:47 -0700

    added assertion to existing user creation test
    we are expecting to see the status code of 400 as a bad request since the user credentials are already taken

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -44,4 +44,5 @@ class PublicUserApiTests(TestCase):
         create_user(**payload)
 
         response = self.client.post(CREATE_USER_URL, payload)
-        
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
