2c6de9b91444164111a4a5058c601cd6d1641cce
Paahn <pjhatzis@gmail.com>
2020-08-21 20:28:20 -0700

    added assertions to token non-creation test
    check token isnt in response data and we got a bad request

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -99,3 +99,6 @@ class PublicUserApiTests(TestCase):
             'name': 'OMG whiz'
         }
         response = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', response.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
