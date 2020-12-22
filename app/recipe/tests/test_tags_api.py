b00a6fff7bcca66e0c95185d4b640f587fa4b0ea
Paahn <pjhatzis@gmail.com>
2020-12-21 18:35:08 -0800

    added assertion for status code to be 401 unauthorized
    this test comes into effect if you havent logged in and try to retrieve tags

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -18,4 +18,6 @@ class PublicTagsApiTests(TestCase):
 
     def test_login_required(self):
         """Test that login is required to retrieve tags"""
-        res = self.client.get(TAGS_URLS)
+        response = self.client.get(TAGS_URLS)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
