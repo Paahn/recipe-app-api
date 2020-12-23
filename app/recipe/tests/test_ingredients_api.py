94101867daea401fe296ade5f3c210eabeb114ee
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-22 22:22:04 -0800

    added assertion to check first ingredient's name

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -59,3 +59,4 @@ class PrivateIngredientsApiTests(TestCase):
 
     self.assertEqual(response.status_code, status.HTTP_200_OK)
     self.assertEqual(len(response.data), 1)
+    self.assertEqual(response.data[0]['name'], ingredient.name)
