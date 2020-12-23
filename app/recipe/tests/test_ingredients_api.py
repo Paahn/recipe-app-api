36c84ed61a5d35eb40e1cf80c5886c90ec41dfa8
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-22 22:46:20 -0800

    fixed typo and flake8 demands so all tests pass

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -10,53 +10,57 @@ from recipe.serializers import IngredientSerializer
 
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
+
 class PublicIngredientsApiTests(TestCase):
-  """Test the publicly available ingredients API"""
+    """Test the publicly available ingredients API"""
 
-  def setUp(self):
-    self.client = APIClient()
+    def setUp(self):
+        self.client = APIClient()
 
-  def test_login_required(self):
-    """Test that login is required to access this endpoint"""
-    response = self.client.get(INGREDIENTS_URL)
+    def test_login_required(self):
+        """Test that login is required to access this endpoint"""
+        response = self.client.get(INGREDIENTS_URL)
 
-    self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateIngredientsApiTests(TestCase):
-  """Test the private ingredients API"""
+    """Test the private ingredients API"""
 
-  def setUp(self):
-    self.client = APIClient()
-    self.user = get_user_model().objects.create_user(
-      'borat@verynice.kz',
-      'pamelasmallerthan3'
-    )
-    self.client.force_authenticate(self.user)
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+          'borat@verynice.kz',
+          'pamelasmallerthan3'
+        )
+        self.client.force_authenticate(self.user)
 
-  def test_retrieve_ingredients_list(self):
-    """Test retrieving a list of ingredients for an authenticated user"""
-    Ingredient.objects.create(user=self.user, name='A very nice potato')
-    Ingredient.objects.create(user=self.user, name='Wife milk cheese')
+    def test_retrieve_ingredients_list(self):
+        """Test retrieving a list of ingredients for an authenticated user"""
+        Ingredient.objects.create(user=self.user, name='A very nice potato')
+        Ingredient.objects.create(user=self.user, name='Wife milk cheese')
 
-    response = self.client.get(INGREDIENTS_URL)
+        response = self.client.get(INGREDIENTS_URL)
 
-    ingredients = Ingredient.objects.all().order_by('-name')
-    serializer = IngredientSerializer(ingredients, many=True)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(response.data, serializer.data)
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
 
-  def test _ingredients_limited_to_user(self):
-    """Test that ingredients for the authenticated user are returned"""
-    user2 = get_user_model().objects.create_user(
-      'nursultantulyakbay@yahoo.com',
-      'iambest'
-    )
-    Ingredient.objects.create(user=user2, name='Lard')
-    ingredient = Ingredient.objects.create(user=self.user, name='Brown Rice')
+    def test_ingredients_limited_to_user(self):
+        """Test that ingredients for the authenticated user are returned"""
+        user2 = get_user_model().objects.create_user(
+          'nursultantulyakbay@yahoo.com',
+          'iambest'
+        )
+        Ingredient.objects.create(user=user2, name='Lard')
+        ingredient = Ingredient.objects.create(
+          user=self.user,
+          name='Brown Rice'
+        )
 
-    response = self.client.get(INGREDIENTS_URL)
+        response = self.client.get(INGREDIENTS_URL)
 
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(len(response.data), 1)
-    self.assertEqual(response.data[0]['name'], ingredient.name)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], ingredient.name)
