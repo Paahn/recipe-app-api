c53676383f63bed15b6e92e1cff6c8dc8ce26beb
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-23 17:18:06 -0800

    adding test to test creating a new ingredient succesfully, make post request to the url

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -64,3 +64,8 @@ class PrivateIngredientsApiTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+      """Test creating a new ingredient successfully"""
+      payload = {'name': 'Butter'}
+      self.client.post(INGREDIENTS_URL, payload)
