3b394f532a7f672fab5523dc7f77561b39270150
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-23 17:23:33 -0800

    added test to catch invalid attempt to create an ingredient, withan empty string as name

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -75,3 +75,10 @@ class PrivateIngredientsApiTests(TestCase):
         name=payload['name'],
       ).exists()
       self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+      """Test creating an ingredient with invalid payload, which will fail"""
+      payload = {'name': ''}
+      response = self.client.post(INGREDIENTS_URL, payload)
+
+      self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
