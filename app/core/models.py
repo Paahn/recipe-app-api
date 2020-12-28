94fa243998aaf7f1151de64d34472b422b8ee35e
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-28 11:50:14 -0800

    added more fields to Recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -68,3 +68,7 @@ class Recipe(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
