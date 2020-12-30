bfa069727e4dc6201831b2052a192c7838e14aff
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-29 17:58:05 -0800

    test filtering only the assigned tags. sending a GET request to the API asking for assigned_only as 1, which will evaluate to true

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import Tag, Recipe
 from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
@@ -78,3 +78,17 @@ class PrivateTagsApiTests(TestCase):
         payload = {'name': ''}
         response = self.client.post(TAGS_URL, payload)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_tags_assigned_to_recipes(self):
+        """Test filtering only the tags that are assigned to recipes"""
+        tag1 = Tag.objects.create(user=self.user, name='Breakfast')
+        tag2 = Tag.objects.create(user=self.user, name='Dinner')
+        recipe = Recipe.objects.create(
+            title='Egg, Spam, Sausage and Spam',
+            time_minutes=15,
+            price=5.00,
+            user=self.user
+        )
+        recipe.tags.add(tag1)
+
+        response = self.client.get(TAGS_URL, {'assigned_only': 1})
