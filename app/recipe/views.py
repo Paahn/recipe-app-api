388e5a3e74d09fac1d5e577bb1ce118829ff2121
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-12-29 18:33:02 -0800

    we convert the assigned only to integer first and then to boolean to avoid confusion with strings always evaluating to true

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -17,6 +17,9 @@ class BaseRecipeElementsViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only'))
+        )
         return self.queryset.filter(
             user=self.request.user
             ).order_by('-name')
